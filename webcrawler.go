77c1aaa1a0730ce73858e33e9ba36b8f72630352
Kamil Polakowski <kamilpol00@gmail.com>
2020-09-24 08:00:29 +0200

    latest unpushed changes

--- webcrawler.go
+++ webcrawler.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"github.com/gocolly/colly"
+	"github.com/gocolly/colly/queue"
 	"fmt"
 	"github.com/KMPolakowski/diploradar/webcrawler/db"
+	"strings"
 )
 
 func main() {
-	query := "SELECT website FROM foreign_ministries WHERE is_english = 1;"
+	query := "SELECT website FROM foreign_ministries WHERE is_english = 1 AND website IS NOT NULL;"
 	rows, err :=  db.Db.Query(query)
 	
 	defer rows.Close()
@@ -15,21 +17,58 @@ func main() {
 		fmt.Println(err.Error())
 	}
 
-	var users []string
+	var urls []string
 
 	for rows.Next() {
-		var user string
+		var url string
 
-		err := rows.Scan(&user)
+		err := rows.Scan(&url)
 
 		if err != nil {
 			fmt.Println(err.Error())
+			continue
 		}
 
-		users = append(users, user)
+		urls = append(urls, url)
 	}
 
-	fmt.Println(users)
+	q, _ := queue.New(
+						len(urls),
+						&queue.InMemoryQueueStorage{MaxSize:len(urls)},
+				)
 
-	colly.NewCollector(colly.AllowURLRevisit())
+	for _, url := range urls {
+		q.AddURL(url)
+	}
+
+	c := colly.NewCollector(
+	)
+
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("visiting", r.URL)
+	})
+
+	c.OnHTML("p", func(e *colly.HTMLElement) {
+		isInteresting(e)
+	})
+
+	c.OnHTML("span", func(e *colly.HTMLElement) {
+		isInteresting(e) 
+	})
+
+	q.Run(c)
+}
+
+func isInteresting(e *colly.HTMLElement) bool {
+	text := strings.ToUpper(e.Text)
+	words := strings.Fields(text)
+
+	for _, v := range words {
+		if v == "PRESIDENT" || v == "MINISTER" {
+			fmt.Println(e.Text)
+			return true
+		}
+	}
+
+	return false
 }
